0a33b52823c6bf82bc92455b2448abae7bb43ad9
RomaP28 <posohovrom@icloud.com>
2022-02-23 13:01:45 +0200

    Ready to Go

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
 window.onload = init;
 
 function init() {
+  console.log(processGame.getRandomNum());
   document.querySelectorAll('button').forEach((item) => item.addEventListener('click', (e) => processGame.getWinner(e.target.value)));
-  processGame.getRandomNum();
 }
 
 const processGame = {
@@ -14,9 +14,11 @@ const processGame = {
   },
   getWinner(userChoise) {
     if (this.random === +userChoise) {
-      this.value = 'DRAW!'
+      this.value = 'DRAW!';
+      this.color = 'item-yellow-style'
     } else if (this.random === +userChoise - 1 || this.random === 2 && +userChoise === 0) {
       this.value = 'You have WON!';
+      this.color = 'item-green-style'
       this.userScores++;
     } else if (userChoise === 'reset') {
       this.round = 1;
@@ -25,12 +27,13 @@ const processGame = {
       return displayLog.hideResult();
     } else {
       this.value = 'You have LOST!';
+      this.color = 'item-red-style'
       this.computerScores++;
     }
     this.checkWinner(userChoise);
     this.round++;
     this.getRandomNum();
-    displayLog.showResult(this.result);
+    displayLog.showResult(this.result, this.color);
   },
   getName(value) {
     switch (value) {
@@ -46,9 +49,11 @@ const processGame = {
   checkWinner(userChoise) {
     if (this.computerScores === 3) {
       this.result = 'Computer Win';
+      this.color = 'item-red-style'
       this.resetScores();
     } else if (this.userScores === 3) {
       this.result = 'Congratulations! You Win!';
+      this.color = 'item-green-style'
       this.resetScores();
     } else {
       this.result = `Round ${this.round}, ${this.getName(this.random)} vs. ${this.getName(+userChoise)}, ${this.value}`;
@@ -61,9 +66,10 @@ const processGame = {
 };
 
 const displayLog = {
-  showResult(str) {
+  showResult(str, color) {
     const log = document.getElementById('log');
     const p = document.createElement('p');
+    p.setAttribute('class', color)
     log.appendChild(p);
     p.innerHTML = str;
     log.scrollTop = 9999;
